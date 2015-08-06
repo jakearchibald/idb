2e322166697cf544a613cec76de72279080a9806
Jake Archibald <jaffathecake@gmail.com>
2015-08-06 15:29:49 +0100

    More silly testing (I will write serious tests)

--- test/idb.js
+++ test/idb.js
@@ -2,9 +2,7 @@ import "babelify/node_modules/babel-core/node_modules/regenerator/runtime";
 import assert from "assert";
 
 describe('IDB interface', () => {
-  before(() => {
-    return IDB.delete('tmp-db');
-  });
+  before(done => IDB.delete('tmp-db').then(done));
 
   it('exists on window', () => {
     assert('IDB' in self);
@@ -16,22 +14,38 @@ describe('IDB interface', () => {
   });
 
   it('stuff', async () => {
+    // Open the database
     let db = await IDB.open('tmp-db', 1, upgradeDb => {
       switch (upgradeDb.oldVersion) {
         case 0:
-          upgradeDb.createObjectStore("my-store", {
+          upgradeDb.createObjectStore("list-of-things", {
             keyPath: ''
           });
+          upgradeDb.createObjectStore('key-val');
       }
     });
 
-    let tx = db.transaction('my-store', 'readwrite');
-    let store = tx.objectStore('my-store');
+    // Add some things to the list
+    let tx = db.transaction('list-of-things', 'readwrite');
+    let store = tx.objectStore('list-of-things');
     store.put('foo');
     store.put('bar');
     store.put('baz');
 
-    let cursor = await db.transaction('my-store').objectStore('my-store').openCursor();
+    // Add a "hello":"world" to the key-val store
+    db.transaction('key-val', 'readwrite')
+      .objectStore('key-val')
+      .put('world', 'hello');
+
+    // Add the value of "hello" to the list
+    tx = db.transaction(['key-val', 'list-of-things'], 'readwrite');
+    tx.objectStore('list-of-things').put(
+      await tx.objectStore('key-val').get('hello')
+    );
+
+    // Itterate over the list
+    let cursor = await db.transaction('list-of-things')
+      .objectStore('list-of-things').openCursor();
 
     while (cursor) {
       console.log(cursor.value);
