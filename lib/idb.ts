2dd193c694f6498286732f5e8d09f7735f16e930
Jake Archibald <jaffathecake@gmail.com>
2019-01-11 17:10:14 +0000

    Initial stab at things, but cursors are still failing

---
+++ lib/idb.ts
@@ -0,0 +1,167 @@
+type IDBProxyable = IDBDatabase | IDBObjectStore | IDBIndex | IDBCursor | IDBTransaction;
+type IDBCursorAdvanceMethod =
+  IDBCursor['advance'] | IDBCursor['continue'] | IDBCursor['continuePrimaryKey'];
+
+const idbProxyableTypes = [IDBDatabase, IDBObjectStore, IDBIndex, IDBCursor, IDBTransaction];
+const cursorMethodOverwrites: IDBCursorAdvanceMethod[] = [
+  IDBCursor.prototype.advance,
+  IDBCursor.prototype.continue,
+  IDBCursor.prototype.continuePrimaryKey,
+];
+const cursorRequestMap: WeakMap<IDBCursor, IDBRequest<IDBCursor>> = new WeakMap();
+const transactionDoneMap: WeakMap<IDBTransaction, Promise<void>> = new WeakMap();
+
+type Func = (...args: any[]) => any;
+type Constructor = new (...args: any[]) => any;
+
+function instanceOfAny(object: any, constructors: Constructor[]): boolean {
+  for (const constructor of constructors) {
+    if (object instanceof constructor) return true;
+  }
+  return false;
+}
+
+class RequestPromise<T> extends Promise<T> {
+  static get [Symbol.species]() { return Promise; }
+
+  constructor(
+    public request: IDBRequest,
+    executor:
+      (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void,
+  ) {
+    super(executor);
+  }
+}
+
+function promisifyRequest<T>(request: IDBRequest<T>): RequestPromise<T> {
+  const promise = new RequestPromise(request, (resolve, reject) => {
+    const unlisten = () => {
+      request.removeEventListener('success', success);
+      request.removeEventListener('error', error);
+    };
+    const success = () => {
+      resolve(handleIdbReturnValue(request, request.result));
+      unlisten();
+    };
+    const error = () => {
+      reject(request.error);
+      unlisten();
+    };
+    request.addEventListener('success', success);
+    request.addEventListener('error', error);
+  });
+
+  promise.then((value) => {
+    // Remember the request for later
+    if (value instanceof IDBCursor) {
+      cursorRequestMap.set(value, request as unknown as IDBRequest<IDBCursor>);
+    }
+  });
+
+  return promise as RequestPromise<T>;
+}
+
+function processTransaction(tx: IDBTransaction) {
+  if (transactionDoneMap.has(tx)) return;
+  const done = new Promise<void>((resolve, reject) => {
+    const unlisten = () => {
+      tx.removeEventListener('complete', complete);
+      tx.removeEventListener('error', error);
+      tx.removeEventListener('abort', error);
+    };
+    const complete = () => {
+      resolve();
+      unlisten();
+    };
+    const error = () => {
+      reject(tx.error);
+      unlisten();
+    };
+    tx.addEventListener('complete', complete);
+    tx.addEventListener('error', error);
+    tx.addEventListener('abort', error);
+  });
+
+  transactionDoneMap.set(tx, done);
+}
+
+function handleIdbReturnValue(target: any, value: any) {
+  // Functions are always bound to prevent ILLEGAL INVOCATION errors when thisArg is a proxy.
+  if (typeof value === 'function') return proxyIdbFunction(value.bind(target));
+  if (value instanceof IDBRequest) return promisifyRequest(value);
+  if (value instanceof IDBTransaction) processTransaction(value); // Doesn't return
+  if (instanceOfAny(value, idbProxyableTypes)) return proxyIdbObject(value);
+  return value;
+}
+
+async function handleCursorAdvanceMethod<T extends IDBCursorAdvanceMethod>(
+  cursor: IDBCursor,
+  func: T,
+  args: Parameters<T>,
+): Promise<void> {
+  // @ts-ignore
+  func.apply(cursor, args);
+  const request = cursorRequestMap.get(cursor)!;
+  await promisifyRequest(request);
+}
+
+const idbFunctionHandler: ProxyHandler<any> = {
+  apply(target, thisArg, args) {
+    // Look out for special cursor methods
+    if (thisArg instanceof IDBCursor && cursorMethodOverwrites.includes(target)) {
+      return handleCursorAdvanceMethod(thisArg, target, args);
+    }
+    const value = target.apply(null, args);
+    return handleIdbReturnValue(target, value);
+  },
+};
+
+const idbObjectHandler: ProxyHandler<any> = {
+  get(target, prop) {
+    if (prop === 'done' && target instanceof IDBTransaction) {
+      return transactionDoneMap.get(target);
+    }
+
+    const value = target[prop];
+    return handleIdbReturnValue(target, value);
+  },
+};
+
+function proxyIdbFunction<T extends Func>(target: T): T {
+  return new Proxy(target, idbFunctionHandler);
+}
+
+function proxyIdbObject<T extends IDBProxyable>(target: T): T {
+  return new Proxy(target, idbObjectHandler);
+}
+
+interface OpenDbOptions {
+  upgrade?(database: IDBDatabase, oldVersion: number, newVersion: number | null): void;
+  blocked?(): void;
+}
+
+export function openDb(
+  name: string, version: number, options: OpenDbOptions,
+) {
+  const { blocked, upgrade } = options;
+  const request = indexedDB.open(name, version);
+
+  return new RequestPromise(request, (resolve, reject) => {
+    if (upgrade) {
+      request.addEventListener('upgradeneeded', (event) => {
+        upgrade(proxyIdbObject(request.result), event.oldVersion, event.newVersion);
+      });
+    }
+
+    if (blocked) {
+      request.addEventListener('blocked', () => blocked());
+    }
+
+    promisifyRequest(request).then(proxyIdbObject).then(resolve, reject);
+  });
+}
+
+export function deleteDb(name: string) {
+  const request = indexedDB.deleteDatabase(name);
+  return promisifyRequest(request);
+}
---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from "rollup-plugin-terser";
+
+const esm = {
+  plugins: [ typescript({ useTsconfigDeclarationDir: false }) ],
+  input: 'lib/idb.ts',
+  output: {
+    file: 'build/idb.mjs',
+    format: 'esm'
+  },
+};
+
+const iffe = {
+  input: 'build/idb.mjs',
+  output: {
+    file: 'build/idb.js',
+    format: 'iife',
+    name: 'idb'
+  },
+};
+
+const iffeMin = {
+  input: 'build/idb.mjs',
+  plugins: [
+    terser({
+      compress: { ecma: 6 },
+    })
+  ],
+  output: {
+    file: 'build/idb-min.js',
+    format: 'iife',
+    name: 'idb'
+  },
+};
+
+export default [esm, iffe, iffeMin];
