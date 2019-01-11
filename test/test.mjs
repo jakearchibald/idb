e41efff72be022abb11429f6464bfeb51007576c
Jake Archibald <jaffathecake@gmail.com>
2019-01-11 17:10:14 +0000

    Initial stab at things, but cursors are still failing

---
+++ test/test.mjs
@@ -0,0 +1,125 @@
+import '../node_modules/mocha/mocha.js';
+import '../node_modules/chai/chai.js';
+import { /** @type typeof openDb */ openDb, deleteDb } from '../build/idb.mjs';
+
+mocha.setup('bdd');
+const { assert, expect } = chai;
+
+describe('deleteDb setup', () => {
+  it('is a function', () => expect(deleteDb).to.be.a('function'));
+  it('returns a promise for void', async () => {
+    const val = deleteDb('test');
+    expect(val).to.be.a('promise');
+    const resolvedVal = await val;
+    expect(resolvedVal).to.be.undefined;
+  });
+});
+
+describe('database', () => {
+  /**
+   * @type IDBDatabase
+   */
+  let db;
+
+  it('can be opened', async () => {
+    expect(openDb).to.be.a('function');
+
+    let upgradeCalled = false;
+
+    db = await openDb('test', 1, {
+      upgrade(db, oldVersion, newVersion) {
+        upgradeCalled = true;
+        expect(db).to.be.an.instanceOf(IDBDatabase);
+        expect(oldVersion).to.eq(0);
+        expect(newVersion).to.eq(1);
+
+        db.createObjectStore('test-store');
+      },
+    });
+
+    expect(upgradeCalled).to.be.true;
+
+    expect(db).to.be.an.instanceOf(IDBDatabase);
+    expect(db.name).to.eq('test');
+    expect([...db.objectStoreNames]).to.eql(['test-store']);
+  });
+
+  it('can add items & confirm with done promise', async () => {
+    const tx = db.transaction('test-store', 'readwrite');
+    const store = tx.objectStore('test-store');
+    const requestPromise = store.add('foo', 'bar');
+
+    expect(requestPromise).to.be.a('promise');
+    expect(requestPromise.request).to.be.instanceOf(IDBRequest);
+    expect(tx.done).to.be.a('promise');
+    await tx.done;
+  });
+
+  it('can get items via promises', async () => {
+    const tx = db.transaction('test-store');
+    const store = tx.objectStore('test-store');
+    const result = await store.get('bar');
+    expect(result).to.be.eq('foo');
+  });
+
+  it('can upgrade with indexes, and handles blocking', async () => {
+    let upgradeCalled = false;
+    let blockedCalled = false;
+    const oldDb = db;
+
+    db = await openDb('test', 2, {
+      upgrade(db, oldVersion, newVersion) {
+        upgradeCalled = true;
+        expect(oldVersion).to.eq(1);
+        expect(newVersion).to.eq(2);
+
+        const store = db.createObjectStore('index-store', { keyPath: 'id' });
+        store.createIndex('order', 'order');
+        store.put({ id: 1, order: 3, val: 'hello' });
+        store.put({ id: 2, order: 2, val: 'world' });
+        store.put({ id: 3, order: 1, val: 'foobar' });
+      },
+      blocked() {
+        blockedCalled = true;
+        oldDb.close();
+      },
+    });
+
+    expect(upgradeCalled).to.be.true;
+    expect(blockedCalled).to.be.true;
+  });
+
+  it('can get items via index', async () => {
+    const tx = db.transaction('index-store');
+    const store = tx.objectStore('index-store');
+    const index = store.index('order');
+
+    const all = await index.getAll();
+
+    expect(all).to.eql([
+      { id: 3, order: 1, val: 'foobar' },
+      { id: 2, order: 2, val: 'world' },
+      { id: 1, order: 3, val: 'hello' },
+    ]);
+  });
+
+  it('can cursor over items', async () => {
+    const tx = db.transaction('index-store', 'readwrite');
+    const store = tx.objectStore('index-store');
+
+    let storeCursor = await store.openCursor();
+    const storeVals = [];
+
+    while (storeCursor) {
+      storeVals.push(storeCursor.value.val);
+      storeCursor = await storeCursor.continue();
+    }
+
+    expect(storeVals).to.eql(['hello', 'world', 'foobar']);
+
+    const index = store.index('order');
+    const indexVals = [];
+  });
+});
+
+mocha.run();
