f515af876de4b3d4975c34cb9dee6e8dd68c79ad
Jake Archibald <jaffathecake@gmail.com>
2019-01-14 09:25:15 +0000

    Comments and docs

--- lib/request-promise.ts
+++ lib/request-promise.ts
@@ -1,11 +1,17 @@
 export default class RequestPromise<T> extends Promise<T> {
+  // The request property only exists for this instance. Resolving this promise, eg calling .then
+  // will return a regular Promise.
   static get [Symbol.species]() { return Promise; }
 
+  /** The original IDBRequest for this action. */
+  public request: IDBRequest;
+
   constructor(
-    public request: IDBRequest,
+    request: IDBRequest,
     executor:
       (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void,
   ) {
     super(executor);
+    this.request = request;
   }
 }
--- lib/transform-value.ts
+++ lib/transform-value.ts
@@ -1,8 +1,9 @@
 import RequestPromise from './request-promise';
 
-type IDBProxyable = IDBDatabase | IDBObjectStore | IDBIndex | IDBCursor | IDBTransaction;
 type IDBCursorAdvanceMethod =
   IDBCursor['advance'] | IDBCursor['continue'] | IDBCursor['continuePrimaryKey'];
+type Constructor = new (...args: any[]) => any;
+type Func = (...args: any[]) => any;
 
 const idbProxyableTypes = [IDBDatabase, IDBObjectStore, IDBIndex, IDBCursor, IDBTransaction];
 const cursorAdvanceMethods: IDBCursorAdvanceMethod[] = [
@@ -15,15 +16,8 @@ const transactionDoneMap: WeakMap<IDBTransaction, Promise<void>> = new WeakMap()
 const transformCache = new WeakMap();
 const reverseTransformCache = new WeakMap();
 
-type Constructor = new (...args: any[]) => any;
-type Func = (...args: any[]) => any;
-
-function instanceOfAny(object: any, constructors: Constructor[]): boolean {
-  for (const constructor of constructors) {
-    if (object instanceof constructor) return true;
-  }
-  return false;
-}
+const instanceOfAny = (object: any, constructors: Constructor[]): boolean =>
+  constructors.some(c => object instanceof c);
 
 function promisifyRequest<T>(request: IDBRequest<T>): RequestPromise<T> {
   const promise = new RequestPromise(request, (resolve, reject) => {
@@ -44,7 +38,8 @@ function promisifyRequest<T>(request: IDBRequest<T>): RequestPromise<T> {
   });
 
   promise.then((value) => {
-    // Remember the request for later
+    // Since cursoring reuses the IDBRequest (*sigh*), we cache it for later retrieval
+    // (see wrapFunction).
     if (value instanceof IDBCursor) {
       cursorRequestMap.set(value, request as unknown as IDBRequest<IDBCursor>);
     }
@@ -53,8 +48,10 @@ function promisifyRequest<T>(request: IDBRequest<T>): RequestPromise<T> {
   return promise as RequestPromise<T>;
 }
 
-function processTransaction(tx: IDBTransaction) {
+function cacheDonePromiseForTransaction(tx: IDBTransaction) {
+  // Early bail if we've already created a done promise for this transaction.
   if (transactionDoneMap.has(tx)) return;
+
   const done = new Promise<void>((resolve, reject) => {
     const unlisten = () => {
       tx.removeEventListener('complete', complete);
@@ -74,27 +71,30 @@ function processTransaction(tx: IDBTransaction) {
     tx.addEventListener('abort', error);
   });
 
+  // Cache it for later retrieval.
   transactionDoneMap.set(tx, done);
 }
 
 const idbObjectHandler: ProxyHandler<any> = {
   get(target, prop) {
-    if (prop === 'done' && target instanceof IDBTransaction) {
-      return transactionDoneMap.get(target);
-    }
-
-    const value = target[prop];
-    return transformIdbValue(value, target);
+    // Special handling for transaction.done.
+    if (prop === 'done' && target instanceof IDBTransaction) return transactionDoneMap.get(target);
+    // Else transform whatever we get back.
+    return transformIdbValue(target[prop]);
   },
 };
 
-function proxyIdbObject<T extends IDBProxyable>(target: T): T {
-  return new Proxy(target, idbObjectHandler);
-}
+function wrapFunction<T extends Func>(func: T): Function {
+  // Due to expected object equality (which is enforced by the caching in transformIdbValue), we
+  // only create one new func per func, so we can't refer to `parent` inside the function, as it may
+  // be different at call time.
 
-function wrapFunction<T extends Func>(func: T, parent: any): Function {
-  // Cursor methods are special
-  if (parent instanceof IDBCursor && cursorAdvanceMethods.includes(func)) {
+  // Cursor methods are special, as the behaviour is a little more different to standard IDB. In
+  // IDB, you advance the cursor and wait for a new 'success' on the IDBRequest that gave you the
+  // cursor. It's kinda like a promise that can resolve with many values. That doesn't make sense
+  // with real promises, so each advance methods returns a new promise for the cursor object, or
+  // undefined if the end of the cursor has been reached.
+  if (cursorAdvanceMethods.includes(func)) {
     return function (this: IDBCursor, ...args: Parameters<T>) {
       // Calling the original function with the proxy as 'this' causes ILLEGAL INVOCATION, so we use
       // the original object.
@@ -105,29 +105,45 @@ function wrapFunction<T extends Func>(func: T, parent: any): Function {
     };
   }
 
-  return function (this: typeof parent, ...args: Parameters<T>) {
+  return function (this: any, ...args: Parameters<T>) {
+    // Calling the original function with the proxy as 'this' causes ILLEGAL INVOCATION, so we use
+    // the original object.
     const originalParent = reverseTransformCache.get(this);
     const value = func.apply(originalParent, args);
     return transformIdbValue(value);
   };
 }
 
-function transformCachableValue(value: any, parent: any = null): any {
-  if (typeof value === 'function') return wrapFunction(value, parent);
+function transformCachableValue(value: any): any {
+  if (typeof value === 'function') return wrapFunction(value);
 
-  if (value instanceof IDBTransaction) processTransaction(value); // Doesn't return
-  if (instanceOfAny(value, idbProxyableTypes)) return proxyIdbObject(value);
+  // This doesn't return, it just creates a 'done' promise for the transaction,
+  // which is later returned for transaction.done (see idbObjectHandler).
+  if (value instanceof IDBTransaction) cacheDonePromiseForTransaction(value);
+
+  if (instanceOfAny(value, idbProxyableTypes)) return new Proxy(value, idbObjectHandler);
+
+  // Return the same value back if we're not going to transform it.
   return value;
 }
 
-export default function transformIdbValue(value: any, parent: any = null): any {
-  // We can generate multiple promises from a single IDBRequest (when cursoring), so these can't be
-  // cached.
+/**
+ * Enhance an object/function with library helpers.
+ *
+ * @param value The thing to enhance.
+ */
+export default function transformIdbValue(value: any): any {
+  // We sometimes generate multiple promises from a single IDBRequest (eg when cursoring), because
+  // IDB is weird and a single IDBRequest can yield many responses, so these can't be cached.
   if (value instanceof IDBRequest) return promisifyRequest(value);
 
+  // If we've already transformed this value before, reuse the transformed value.
+  // This is faster, but it also provides object equality.
   if (transformCache.has(value)) return transformCache.get(value);
-  const newValue = transformCachableValue(value, parent);
+  const newValue = transformCachableValue(value);
 
+  // Not all types are transformed.
+  // These may be primitive types, so they can't be WeakMap keys.
   if (newValue !== value) {
     transformCache.set(value, newValue);
     reverseTransformCache.set(newValue, value);
