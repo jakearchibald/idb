6c1e154776849ccc3b18f6a4a15a2c10e5ff75c4
Jake Archibald <jaffathecake@gmail.com>
2019-04-04 10:12:26 +0100

    Async ittr CJS should point to CJS build files. Fixes #98

--- with-async-ittr-cjs.js
+++ with-async-ittr-cjs.js
@@ -1,10 +1,7 @@
 'use strict';
-
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var index_js = require('./build/esm/index.js');
-require('./build/esm/async-iterators.js');
-
-
+var index_js = require('./build/cjs/index.js');
+require('./build/cjs/async-iterators.js');
 
 Object.keys(index_js).forEach(function (key) { exports[key] = index_js[key]; });
